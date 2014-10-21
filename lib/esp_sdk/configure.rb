47961818fbaf96106a8eac84869f9a411a66adb6
amedeiros <amedeiros0920@gmail.com>
2014-10-21 14:14:43 -0400

    Fix end point

--- lib/esp_sdk/configure.rb
+++ lib/esp_sdk/configure.rb
@@ -13,14 +13,14 @@ module EspSdk
 
       if ENV['RAILS_ENV'].present? || !!defined?(Rails)
         if ENV['RAILS_ENV'] == 'release'
-          @uri = 'https://sig-api-release.evident.io'
+          @uri = 'https://sig-api-release.evident.io/api'
         elsif (ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test') || (!!defined?(Rails) && ( Rails.env.development? || Rails.env.test? ))
           @uri = 'http://0.0.0.0:3001/api'
         else
-          @uri = 'https://sig-api.evident.io'
+          @uri = 'https://sig-api.evident.io/api'
         end
       else
-        @uri = 'https://sig-api.evident.io'
+        @uri = 'https://sig-api.evident.io/api'
       end
     end
 
