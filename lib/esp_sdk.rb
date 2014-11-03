bcc836faecb0d0e6da9c79e6c8c1ae06ba75aab2
amedeiros <amedeiros0920@gmail.com>
2014-11-03 08:32:01 -0500

    environment update

--- lib/esp_sdk.rb
+++ lib/esp_sdk.rb
@@ -16,3 +16,31 @@ require_relative 'esp_sdk/end_points/contact_requests'
 require_relative 'esp_sdk/end_points/services'
 require_relative 'esp_sdk/api'
 require_relative 'esp_sdk/exceptions'
+
+module EspSdk
+  # Default environment is production
+  def self.env
+    @env ||= (ENV['ESP_ENV'] || ENV['RAILS_ENV'] || :production).to_sym
+  end
+
+  # Production environment query method
+  def self.production?
+    env == :production
+  end
+
+  # Release environment query method
+  def self.release?
+    env == :release
+  end
+
+  # Development environment query method
+  def self.development?
+    env == :development
+  end
+
+  # Test environment query method
+  def self.test?
+    env == :test
+  end
+end
+
--- lib/esp_sdk/configure.rb
+++ lib/esp_sdk/configure.rb
@@ -8,24 +8,16 @@ module EspSdk
       @token    = options[:token]
     end
 
-    def self.end_point
+    def uri
       return @uri if @uri.present?
 
-      if ENV['RAILS_ENV'].present? || !!defined?(Rails)
-        if ENV['RAILS_ENV'] == 'release'
-          @uri = 'https://sig-api-release.evident.io/api'
-        elsif (ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test') || (!!defined?(Rails) && ( Rails.env.development? || Rails.env.test? ))
-          @uri = 'http://0.0.0.0:3001/api'
-        else
-          @uri = 'https://sig-api.evident.io/api'
-        end
+      if EspSdk.production?
+        @uri = 'https://api.evident.io/api'
+      elsif EspSdk.release?
+        @uri = 'https://api-rel.evident.io/api'
       else
-        @uri = 'https://sig-api.evident.io/api'
+        @uri = 'http://0.0.0.0:3001/api'
       end
-    end
-
-    def uri
-      Configure.end_point
     end
   end
 end
