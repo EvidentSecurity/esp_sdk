909e19fd9ecbb92ad4a8761ef84bbadecd7da93c
kevintyll <kevintyll@gmail.com>
2015-08-31 15:45:21 -0400

    FS-504 - change the endpoint

--- lib/esp_sdk/resources/dashboard.rb
+++ lib/esp_sdk/resources/dashboard.rb
@@ -2,13 +2,13 @@ module ESP
   class Dashboard < ESP::Resource
     def self.recent
       # call find_every directly since find is overriden/not implemented
-      find_every from: "#{prefix}dashboard.#{format.extension}"
+      find_every from: "#{prefix}dashboard/recent.#{format.extension}"
     end
 
     def self.timewarp(start_of_window)
       raise ArgumentError, "expected a start time" unless start_of_window.present? && start_of_window.is_a?(Time)
       # call find_every directly since find is overriden/not implemented
-      find_every(from: "#{prefix}dashboard/timewarp.#{format.extension}", params: { time: start_of_window.to_i })
+      find_every(from: "#{prefix}dashboard/timewarp.#{format.extension}", params: { filter: { time: start_of_window.to_i } })
     end
 
     def self.find(*)
