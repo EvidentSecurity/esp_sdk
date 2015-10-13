f15ed86e65eee43976cb49e2fee8ca1edbbfe0b0
kevintyll <kevintyll@gmail.com>
2015-10-13 13:33:01 -0400

    Putting the cart before the horse.

--- test/esp/credentials_test.rb
+++ test/esp/credentials_test.rb
@@ -4,13 +4,13 @@ module ESP
   class CredentialsTest < ActiveSupport::TestCase
     context ESP::Credentials do
       setup do
+        ESP::Credentials.access_key_id = nil
+        ESP::Credentials.secret_access_key = nil
         @original_access_key_id = ENV['ESP_ACCESS_KEY_ID']
         @original_secret_access_key = ENV['ESP_SECRET_ACCESS_KEY']
       end
 
       teardown do
-        ESP::Credentials.access_key_id = nil
-        ESP::Credentials.secret_access_key = nil
         ENV['ESP_ACCESS_KEY_ID'] = @original_access_key_id
         ENV['ESP_SECRET_ACCESS_KEY'] = @original_secret_access_key
       end
