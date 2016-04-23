ab6b69d46aea665893255fbd64e195b1cd3b3bb0
Johnathan Ludwig <john@johnathanludwig.com>
2016-04-22 20:20:27 -0400

    Add API endpoint for exporting reports to integration

---
+++ test/esp/integration/report_export_integration_integration_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+
+module ESP::Integration
+  class Report
+    module Export
+      class IntegrationTest < ESP::Integration::TestCase
+        context ESP::Report::Export::Integration do
+          context 'live calls' do
+            context '#create' do
+              should 'return regions' do
+                report = ESP::Report.last
+                fail "Live DB does not have any reports.  Add a report with stats and run tests again." if report.blank?
+
+                response = ESP::Report::Export::Integration.create(integration_id: 1, report_ids: [report.id])
+
+                assert_predicate response.errors, :blank?
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
