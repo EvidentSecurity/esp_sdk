255eb7c071a630528478d77c6cb67e18305dee8b
Johnathan Ludwig <john@johnathanludwig.com>
2017-02-15 12:41:23 -0500

    Put test back

--- test/esp/integration/json_api_format_integration_test.rb
+++ test/esp/integration/json_api_format_integration_test.rb
@@ -43,6 +43,21 @@ module ESP::Integration
             assert_not_nil user.sub_organization_ids
           end
         end
+
+        context ActiveResource::ConnectionError do
+          context 'live calls' do
+            context "with ESP::Team" do
+              context '.initialize' do
+                should 'parse the error and return a descriptive error message' do
+                  error = assert_raises ActiveResource::BadRequest do
+                    ESP::Team.create
+                  end
+                  assert_equal 'Failed.  Response code = 400.  Response message = param is missing or the value is empty: attributes.', error.message
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
